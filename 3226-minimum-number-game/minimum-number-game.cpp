dc1f18204d1c5747d9bcccfe90414adbc03d8eec
mannlathiya055-jpg <mannlathiya055@gmail.com>
2026-07-27 17:38:50 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (78.04%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.cpp
+++ 3226-minimum-number-game/minimum-number-game.cpp
@@ -2,19 +2,10 @@ class Solution {
 public:
     vector<int> numberGame(vector<int>& v) {
         int n = v.size();
-        vector<int> ans(n);
         sort(v.begin(), v.end());
-        int odd = 0, even = 1; 
-        for(int i=0; i<n; i++) {
-            if(i % 2 == 0) {
-                ans[i] = v[even];
-                even += 2;
-            }
-            else {
-                ans[i] = v[odd];
-                odd += 2;
-            }
+        for(int i=0; i<n; i+=2) {
+            swap(v[i], v[i+1]);
         }
-        return ans;
+        return v;
     }
 };
